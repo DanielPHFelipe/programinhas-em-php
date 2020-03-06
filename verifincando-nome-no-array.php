6266571d9ed10499f75b6e3a6e5c297059fe684a
DanielPHFelipe <danielphfelipe@hotmail.com>
2020-03-06 14:10:06 -0300

    array

---
+++ verifincando-nome-no-array.php
@@ -0,0 +1,23 @@
+<?php 
+
+
+//  verificando se existe tal nome no array
+
+$informeNome = "Boby";
+
+$nomeFuncionario = array("Daniel", "Bruno", "Miriam", "Cesar");
+
+echo ($informeNome == $nomeFuncionario[0])?"Verdadeiro":"Falço";
+
+/* if ($nomeFuncionario[0] == $informeNome) {
+
+    echo "Verdadeiro";
+
+} else {
+
+    echo "Falço";
+}
+*/
+
+
+?>
